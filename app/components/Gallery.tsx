7e177052973696a08e61877368af2df5534d13a1
Aadhil <mohamedaadhilzeo3122@gmail.com>
2026-01-09 16:01:31 +0530

    Add Next.js project

---
+++ app/components/Gallery.tsx
@@ -0,0 +1,13 @@
+export default function Gallery() {
+  return (
+    <div id="gallery" className="py-10 bg-gray-100">
+      <h2 className="text-center text-3xl font-bold mb-8">Gallery</h2>
+
+      <div className="grid grid-cols-3 gap-4 px-10">
+        <img src="/images/gallery1.jpg" />
+        <img src="/images/gallery2.jpg" />
+        <img src="/images/gallery3.jpg" />
+      </div>
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import FeaturesSection from "./components/FeaturesSection";
+import WelcomeSection from "./components/WelcomeSection";
+import Services from "./components/Services";
+import Courses from "./components/Courses";
+import Gallery from "./components/Gallery";
+import Footer from "./components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <Hero />
+      <FeaturesSection />
+      <WelcomeSection />
+      <Services />
+      <Courses />
+      <Gallery />
+      <Footer />
+    </>
+  );
+}
